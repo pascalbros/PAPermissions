19c20fefdee5ade558cc4969ac37a386b82e7cbb
Joe Blau <josephblau@gmail.com>
2016-10-02 12:28:29 -0400

    Added carthage support

---
+++ Example/PAPermissions/PAPermissions.h
@@ -0,0 +1,19 @@
+//
+//  PAPermissions.h
+//  PAPermissions
+//
+//  Created by Joseph Blau on 10/2/16.
+//  Copyright © 2016 Pasquale Ambrosini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PAPermissions.
+FOUNDATION_EXPORT double PAPermissionsVersionNumber;
+
+//! Project version string for PAPermissions.
+FOUNDATION_EXPORT const unsigned char PAPermissionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PAPermissions/PublicHeader.h>
+
+
